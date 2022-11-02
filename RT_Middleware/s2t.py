c6c0ae6b25b44b3d0d34e2de90a5cc30b5cc136f
Ruchi12377 <rappinngu2006@gmail.com>
2022-11-03 00:48:04 +0900

    バグ修正とバッチファイル

--- RT_Middleware/s2t.py
+++ RT_Middleware/s2t.py
@@ -1,13 +1,13 @@
 import os
 import sys
 
-#extract python file of rt component via listdir
+# extract python file of rt component via listdir
 for f in [f for f in os.listdir("./") if os.path.isdir(os.path.join("./", f)) & os.path.isfile("./{0}/{0}.py".format(f)) & ((f in sys.argv) == False)]:
-    r = open("./{0}/{0}.py".format(f), 'r')
+    r = open("./{0}/{0}.py".format(f), 'r', encoding='utf-8')
     c = r.read()
     r.close()
     p = open("./{0}/{0}.py".format(f), 'w')
-    p.write(c.replace('    ', '\t'))
+    p.write(c.replace('	', '\t'))
     p.close()
 
 print("Done")
